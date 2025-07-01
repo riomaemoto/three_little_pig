53b13c7175680abf165c7c5b2f6496d555d3a638
rio maemoto <leo.a1025ark@gmail.com>
2025-07-01 12:12:51 +0900

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import NeonBorder from "@/components/NeonBorder";
 import GlowingLogo from "@/components/GlowingLogo";
 
 export default function Home() {
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import GlowingLogo from "./GlowingLogo";
 
 const navLinks = [
   { href: "/", label: "Home" },
